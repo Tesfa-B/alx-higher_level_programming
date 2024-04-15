d8f2b5476dc4cedf1f9c14e62feda63c125b8dfa
Tesfa-B <Tesfa.B11@proton.me>
2024-04-15 13:53:26 +0100

    Add comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,10 @@
 class MyList(list):
+    """
+    A subclass of list that provides addtional
+    """
     def print_sorted(self):
+        """
+        prints sorted in ascending order
+        """
         sorted_li = sorted(self)
         print(sorted_li)
