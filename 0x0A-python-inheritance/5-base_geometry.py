c6cc79036f43eefae3398acdee7d81400e080368
Tesfa-B <Tesfa.B11@proton.me>
2024-04-15 20:24:32 +0100

    base area

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,2 +1,5 @@
 class BaseGeometry:
+    """
+    empty class
+    """
     pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+class BaseGeometry:
+    """
+    Base class geometry shapes
+    """
+    def area(self):
+        """
+        claculate the area of the geometry shape
+        """
+        raise Exception("area() is not implemented")
